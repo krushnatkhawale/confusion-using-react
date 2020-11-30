09606c05e82a84a01196b2a2b9a29b3e3af6bbcc
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2020-11-30 22:11:01 +0000

    Assignment 2 - functional component, router, media object

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,6 +10,7 @@ import Footer from './Footer';
 import {Switch, Route, Redirect } from 'react-router-dom';
 import Home from './Home';
 import Contact from './Contact';
+import About from './About';
 
 class Main extends Component {
 
@@ -51,6 +52,7 @@ class Main extends Component {
           <Route exact path="/menu" component={()=><Menu dishes={this.state.dishes} /> } />
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/aboutus" component={()=><About leaders={this.state.leaders} /> } />
           <Redirect to="/home" />
         </Switch>
         <Footer />
